63074238968ce479c3da27a03b6d6dcd98a90043
Stanley Nicholas <stannics28@gmail.com>
2024-05-21 15:54:42 +0700

    Initial Commit

---
+++ jamponion-2/jamponion_2App.swift
@@ -0,0 +1,17 @@
+//
+//  jamponion_2App.swift
+//  jamponion-2
+//
+//  Created by Stanley Nicholas on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct jamponion_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
